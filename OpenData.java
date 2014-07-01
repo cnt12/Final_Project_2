0d81e0ae1148a5c94f06bf49202faa479285c77e
cnt12 <cnt1347@gmail.com>
2014-07-02 00:02:24 +0800

    可以按照自己的輸入搜尋老師是否在網頁中
    可以按照自己的輸入搜尋老師是否在網頁中

--- OpenData.java
+++ OpenData.java
@@ -1,3 +1,4 @@
+package com.opendata;
 import java.io.*;
 import java.util.*;
 import org.jsoup.nodes.Document;
@@ -7,19 +8,43 @@ import java.lang.*;
 import java.net.URL;
 
 
+
 public class OpenData {
 
-	public static void main(String[] args)throws IOException {
+	public static void main(String args[])throws IOException {
+		
+		String str1;
+		System.out.println("請輸入你要查詢的課程老師名稱");
+		Scanner sc = new Scanner(System.in);
+		str1 = sc.next();
+		parse_teacher(str1);
+		
+	}
+	
+	 public static void parse_teacher(String input)throws IOException{
+
+		 
 		URL url = new URL("http://140.116.165.74/qry/qry001.php?dept_no=F7");
-		//URL url = new URL("http://www.dgbas.gov.tw/ct.asp?xItem=13213&CtNode=3504&mp=1");
+		
 		Document doc = Jsoup.parse(url, 3000);  
 		Element jsElms = doc.select("table").get(0);  
-		Iterator <Element> iter = jsElms.select("tr").iterator();  
+		Iterator <Element> iter = jsElms.select("td").iterator();  
+		
+		boolean find = false;
+		String mt = null;
 		while(iter.hasNext())  
 		{  
-			System.out.println( iter.next().text());
+			mt = iter.next().text();
+			if(mt.equals(input)){
+				System.out.println(mt+"的確有");
+				find = true;
+				break;
+			}
+				
+			
 		}
-		
+		if(!find)
+			System.out.println("找不到與 "+mt+" 的相關資料");
 	}
 
 }
